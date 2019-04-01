65122ba6531252529199441eac4d08d69a0684a6
Mahdi Ghaznavi <ghaznavi.mahdi@gmail.com>
2019-04-01 18:45:30 +0430

    encoder files completion

---
+++ HP.py
@@ -0,0 +1,6 @@
+prediction_data_set_path = ''
+option_file = 'ELMo/options.json'
+weight_file = 'ELMo/lm_weights.hdf5'
+bath_size = 32
+vocab_file = 'ELMo/vocab-2016-09-10.txt'
+embedding_weight_file = 'ELMo/vocab-2016-09-10.txt'
